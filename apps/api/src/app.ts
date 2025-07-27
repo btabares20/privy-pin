74abef4c5a0e1075bbd1aae8075aac1e969e2277
Bryan Tabares <tabaresbryan39@gmail.com>
2025-07-27 18:41:49 +0800

    added own logging middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+import logger from './middlewares/logger';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(logger);
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({ message: 'Privy Pin API is running! Yey, you can fuck off now!'});
