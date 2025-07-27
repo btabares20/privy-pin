bd565f201e90865efe0f20812065a39dcf72f6ca
Bryan Tabares <tabaresbryan39@gmail.com>
2025-07-27 18:44:48 +0800

    minor changes

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import logger from './middlewares/logger';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 
 app.use(cors());
