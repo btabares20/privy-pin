ba5807bacf0de7c747fc93257711987610a17bce
Bryan Tabares <tabaresbryan39@gmail.com>
2025-07-28 11:42:03 +0800

    added back single create toilet | what's the point of branches if i keep adding more stuff, I will stop creating branches

--- apps/api/src/routes/toiletRoutes.ts
+++ apps/api/src/routes/toiletRoutes.ts
@@ -1,14 +1,15 @@
 import express from 'express';
-import { getToilets, deleteToilet, updateToilet, getToilet, getNearbyToilets, createToiletBatch } from '../controllers/toiletController';
+import { getToilets, deleteToilet, updateToilet, getToilet, getNearbyToilets, createToiletBatch, createToilet } from '../controllers/toiletController';
 
 const router = express.Router();
 
 router.get('/nearby', getNearbyToilets);
+router.post('/batch', createToiletBatch);
 router.get('/', getToilets);
 
 router.get('/:id', getToilet);
 router.patch('/:id', updateToilet);
-router.post('/', createToiletBatch);
+router.post('/', createToilet);
 router.delete('/:id', deleteToilet);
 
 export default router;
